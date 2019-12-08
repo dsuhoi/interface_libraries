f43215d27739ae44776e1830403ef60c07cbe968
DSuhoi <48738655+DSuhoi@users.noreply.github.com>
2019-12-08 22:57:27 +0300

    Update uart.h

--- UART/uart.h
+++ UART/uart.h
@@ -1,16 +1,16 @@
 ﻿/*#####################################################################################
-  ##Lybrary for working with UART. Supports AVR models: atmega(8/16/32/328(p)/644(p))##
+  ##Library for working with UART. Supports AVR models: atmega(8/16/32/328(p)/644(p))##
   ##The desired models can be added from #include <avr/io.h>.		    (c)DSuhoi##      
   #####################################################################################*/
 
 #ifndef __UART_H__
 #define __UART_H__
 
-#define ISR_COUNT 0//parameter of using UART handler and buffer(0-off, 1-on)
+#define ISR_COUNT 0     //parameter of using UART handler and buffer(0-off, 1-on)
 
 #define F_CPU 8000000UL	//frequency of the microcontroller
 #define ELEMENTS 512    //UART buffer size
-//очистка UART буфера
+//clearing the UART buffer
 #define BUF_CLEAR {for(uint16_t i=0; i<ELEMENTS; buffer_uart[i++]=0); index_buf=0;}
 
 //the necessary microcontrollers can be added by yourself
