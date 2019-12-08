059628c6d4c52d79108aa02c834a5b03b4816cf1
DSuhoi <Suhorukov.dan0@gmail.com>
2019-12-08 20:29:00 +0300

    >Changed twi.h and twi.c .

--- TWI/twi.c
+++ TWI/twi.c
@@ -2,29 +2,29 @@
 #include "twi.h"
 
 ///////////////////////////////////////////////////////
-void i2c_init(unsigned long f_scl){		 //data transmission frequency
+void twi_init(unsigned long f_scl){		 //data transmission frequency
 	TWBR = (((F_CPU)/(f_scl)-16)/2);			
 	TWSR = 0;
 }
 //////////////////////////////////////////////////////
-void i2c_start(void){
+void twi_start(void){
 	TWCR = (1<<TWINT)|(1<<TWSTA)|(1<<TWEN);
 	while(!(TWCR & _BV(TWINT)));
 }
 //////////////////////////////////////////////////////
-void i2c_stop(void){
+void twi_stop(void){
 	TWCR = (1<<TWINT)|(1<<TWSTO)|(1<<TWEN);
 	while(!(TWCR & _BV(TWSTO)));
 }
 //////////////////////////////////////////////////////
-uint8_t i2c_write_byte(uint8_t byte){
+uint8_t twi_write_byte(uint8_t byte){
 	TWDR = byte;
 	TWCR = _BV(TWINT) | _BV(TWEN);
 	while(!(TWCR & _BV(TWINT)));
 	return TWSR & 0xF8;
 }
 //////////////////////////////////////////////////////
-uint8_t i2c_read_byte(uint8_t final_byte){
+uint8_t twi_read_byte(uint8_t final_byte){
 	if(!final_byte)
 	TWCR = _BV(TWINT) | _BV(TWEN) | _BV(TWEA);
 	else {
--- TWI/twi.h
+++ TWI/twi.h
@@ -7,10 +7,10 @@
 
 #define F_CPU 8000000UL //frequency of the microcontroller
 
-void i2c_init(unsigned long);	 //initialization twi
-void i2c_start(void);		 //start signal
-void i2c_stop(void);		 //stop signal
-uint8_t i2c_write_byte(uint8_t); //byte transfering
-uint8_t i2c_read_byte(uint8_t);	 //byte recieving (1-receiving the last byte)
+void twi_init(unsigned long);	 //initialization twi
+void twi_start(void);		 //start signal
+void twi_stop(void);		 //stop signal
+uint8_t twi_write_byte(uint8_t); //byte transfering
+uint8_t twi_read_byte(uint8_t);	 //byte recieving (1-receiving the last byte)
 
 #endif
